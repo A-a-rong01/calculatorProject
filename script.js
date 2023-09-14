c535aff5ae9595f749c56f1fe6f97d7292d0aa68
Aaron Guerrero <138899922+A-a-rong01@users.noreply.github.com>
2023-09-13 22:38:17 -0700

    added the JS file as well as the operation buttons, started the JS but need a lot more work

---
+++ script.js
@@ -0,0 +1,23 @@
+// The following are the variables for the numbers
+const numberOne = document.getElementById("numberOne");
+const numberTwo = document.getElementById("numberTwo");
+const numberThree = document.getElementById("numberThree");
+const numberFour = document.getElementById("numberFour");
+const numberFive = document.getElementById("numberFive");
+const numberSix = document.getElementById("numberSix");
+const numberSeven = document.getElementById("numberSeven");
+const numberEight = document.getElementById("numberEight");
+const numberNine = document.getElementById("numberNine");
+const numberZero = document.getElementById("numberZero");
+
+// The following are the variables for enter
+let enterButton = document.getElementById("enterButton");
+let answer = document.getElementById("answer");
+
+
+
+function displayAnswer(){
+    console.log("hello");
+}
+
+enterButton.addEventListener("click", displayAnswer)
